dfbaac9f4ad357462ddcb544b7fcefad11a03075
diegocho <diegoalonzo2001@hotmail.com>
2024-01-02 01:33:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const PostgreSQLAdapter = require('@bot-whatsapp/database/postgres')
+
+/**
+ * Declaramos las conexiones de PostgreSQL
+ */
+
+const POSTGRES_DB_HOST = 'mahmud.db.elephantsql.com'
+const POSTGRES_DB_USER = 'zvespwme'
+const POSTGRES_DB_PASSWORD = 'RZR6xcCdymOifme3egEPfIRkTCBM1Lrh'
+const POSTGRES_DB_NAME = 'zvespwme'
+const POSTGRES_DB_PORT = '5432'
+
+/**
+ * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
+ *
+ *          Menu Principal
+ *           - SubMenu 1
+ *             - Submenu 1.1
+ *           - Submenu 2
+ *             - Submenu 2.1
+ *
+ * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
+ */
+
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+
+const flowDocs = addKeyword(['empleada', 'empleado', 'trabajador']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación sobre los derechos que le corresponden',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowTuto = addKeyword(['empleadora', 'jefa','empleador', 'jefe']).addAnswer(
+    [
+        '🙌 Aquí encontras los deberes que le corresponde',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowGracias = addKeyword(['otro']).addAnswer(
+    [
+        '🚀 Puedes revisar la documentacion correspondiente',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot* de SUNAFIL para los trabajadores domesticos')
+    .addAnswer(
+        [
+            'te comparto los siguientes links para identificarte',
+            '👉 *empleada* para los derechos  que te corresponde',
+            '👉 *jefe*  para ver tus deberes y los deberes que tienes',
+            '👉 *otro* informacion general',
+        ],
+        null,
+        null,
+        [flowDocs, flowGracias, flowTuto, flowDiscord]
+    )
+
+const main = async () => {
+    const adapterDB = new PostgreSQLAdapter({
+        host: POSTGRES_DB_HOST,
+        user: POSTGRES_DB_USER,
+        database: POSTGRES_DB_NAME,
+        password: POSTGRES_DB_PASSWORD,
+        port: POSTGRES_DB_PORT,
+    })
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+    QRPortalWeb()
+}
+
+main()
